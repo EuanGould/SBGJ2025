44a98db08fe0a832db62209f2ab84e69ecae5662
Eunie <euan2.gould@live.uwe.ac.uk>
2025-09-19 15:37:26 +0100

    basic drink and poison logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,75 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.U2D;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 5f;
+    public float drinkless_speed = 5f;
+    public float endrinked_speed = 2.5f;
+    private float speed = 5f;
     private Rigidbody2D rb;
     private Vector2 input;
-    
+    private int drink_stage = 0;
+    private SpriteRenderer sprite_renderer;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite_renderer = GetComponent<SpriteRenderer>();
+
+        ChangeDrinkStage(0);
+    }
+
+    void ChangeDrinkStage(int new_value)
+    {
+        // 0 is no drink
+        // 1 is an unpoisoned drink
+        // 2 is poisoned drink
+
+        // drink stage shouldnt be able to exceed 2 or go lower than 0
+        drink_stage = Mathf.Min(Mathf.Max(new_value, 0), 2);
+        
+        if (drink_stage == 0)
+        {
+            sprite_renderer.color = Color.green;
+            speed = drinkless_speed;
+        }
+        else if (drink_stage == 1)
+        {
+            sprite_renderer.color = Color.yellow;
+            speed = endrinked_speed;
+        }
+        else if (drink_stage == 2)
+        {
+            sprite_renderer.color = Color.red;
+            speed = endrinked_speed;
+        }
+
+    }
+
+    public int GetDrinkStage()
+    {
+        return drink_stage;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.tag + " : " + Time.time);
+        
+        if(col.gameObject.CompareTag("Bar") && GetDrinkStage() == 0)
+        {
+            ChangeDrinkStage(1);
+        }
+        else if (col.gameObject.CompareTag("Poisoner") && GetDrinkStage() == 1)
+        {
+            ChangeDrinkStage(2);
+        }
+        else if (col.gameObject.CompareTag("Incident"))
+        {
+            // we could also put some kind of stun effect here
+            ChangeDrinkStage(0);
+        }
     }
 
     // Update is called once per frame
