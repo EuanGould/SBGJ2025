99db15598b0586d75810e4e9506db3544cd9ef24
elysekenny <elyse2.kenny@live.uwe.ac.uk>
2025-09-24 17:54:49 +0100

    wip start menu
    made a ui manager to control the ui and the game screens, can start game through button

---
+++ Assets/Scripts/UI Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject StartScreen;
+    public GameObject EndScreen;
+
+    private Canvas StartMenu;
+
+    public void Init()
+    {
+        StartMenu = StartScreen.GetComponent<Canvas>();
+    }
+    public void OnStartClick()
+    {
+        StartMenu.enabled = false;
+    }
+
+    public void OnQuitClick()
+    {
+        // Exit the game
+    }
+    void Start()
+    {
+        Init();
+    }
+
+}
